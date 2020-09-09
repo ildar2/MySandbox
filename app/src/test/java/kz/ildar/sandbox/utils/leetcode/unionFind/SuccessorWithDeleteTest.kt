f802e7b06b2418fb2b458f60d3fcf36f84e15bdc
i.ishalin <i.ishalin@infin-it.kz>
2020-09-09 12:50:29 +0600

    coursera algo unionFind exam

---
+++ app/src/test/java/kz/ildar/sandbox/utils/leetcode/unionFind/SuccessorWithDeleteTest.kt
@@ -0,0 +1,37 @@
+package kz.ildar.sandbox.utils.leetcode.unionFind
+
+import org.junit.Assert.*
+import org.junit.Test
+import kotlin.test.assertFailsWith
+
+class SuccessorWithDeleteTest {
+
+    @Test
+    fun testSuccessorRaw() {
+        val successorWithDelete = SuccessorWithDelete(5)
+        assertEquals(2, successorWithDelete.successor(1))
+        assertFailsWith<IllegalArgumentException>(
+            "x: 5 is out of bounds"
+        ) {
+            successorWithDelete.successor(5)
+        }
+        assertEquals(4, successorWithDelete.successor(4))
+    }
+
+    @Test
+    fun testSuccessorDelete() {
+        val successorWithDelete = SuccessorWithDelete(7)
+        assertEquals(5, successorWithDelete.successor(4))
+
+        successorWithDelete.remove(4)
+        assertFailsWith<IllegalArgumentException>(
+            "x: 4 was deleted"
+        ) {
+            successorWithDelete.successor(4)
+        }
+        assertEquals(5, successorWithDelete.successor(3))
+        assertEquals(3, successorWithDelete.successor(2))
+        successorWithDelete.remove(3)
+        assertEquals(5, successorWithDelete.successor(2))
+    }
+}
